c9cfe1ea592058f5ec48fd6c6aee24824074b52a
Graham Garner <46672225+gcgarner@users.noreply.github.com>
2019-10-31 06:40:10 +0200

    Update terminal.sh
    fixed incorrect password path

--- .templates/mosquitto/terminal.sh
+++ .templates/mosquitto/terminal.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 
 echo "you are about to enter the shell for mosquitto"
-echo "to add a password: mosquitto_passwd -c /mosquitto/config/passwd MYUSER"
+echo "to add a password: mosquitto_passwd -c /mosquitto/config/pwfile MYUSER"
 echo "the command will ask for you password and confirm"
 echo "to exit: exit"
 
