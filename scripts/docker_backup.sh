ec2c08dcbb99678b970a01e7d8e7665ff2461723
877dev <46575282+877dev@users.noreply.github.com>
2019-11-10 20:55:52 +0000

    Appended date to docker_backup.sh
    Appended date stamp to backup file, YYYY-MM-DD so they appear in correct order.
    Delete all backup files except the five most recent.
    
    To do:  Dropbox currently gets a copy of all files, look at how to remove older files if possible.

--- scripts/docker_backup.sh
+++ scripts/docker_backup.sh
@@ -17,24 +17,24 @@ fi
 
 echo "compressing stack folders"
 sudo tar -czf \
-	./backups/docker.tar.gz \
+	./backups/"backup-$(date +"%Y-%m-%d").tar.gz" \
 	--exclude=./volumes/influxdb/* \
 	-T list.txt
 
 rm list.txt
 
-echo "backup saved to ./backups/docker.tar.gz"
+echo "backup saved to ./backups/"backup-$(date +"%Y-%m-%d").tar.gz""
 
-du -h ./backups/docker.tar.gz
+du -h ./backups/"backup-$(date +"%Y-%m-%d").tar.gz"
 
 if [ -f ./backups/dropbox ]; then
 	echo "uploading to dropbox"
-	~/Dropbox-Uploader/dropbox_uploader.sh upload ~/IOTstack/backups/docker.tar.gz /IOTstackBU/
+	~/Dropbox-Uploader/dropbox_uploader.sh upload ./backups/"backup-$(date +"%Y-%m-%d").tar.gz" /IOTstackBU/
 fi
 
 if [ -f ./backups/rclone ]; then
 	echo "uploading to Google Drive"
-	rclone -P copy ./backups/docker.tar.gz gdrive:/IOTstackBU/
+	rclone -P copy ./backups/"backup-$(date +"%Y-%m-%d").tar.gz" gdrive:/IOTstackBU/
 fi
 
 popd
