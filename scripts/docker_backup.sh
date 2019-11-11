a799276d0523fa3ab0f3d0d95b0ae0564a061fdd
877dev <46575282+877dev@users.noreply.github.com>
2019-11-11 14:17:20 +0000

    Added DATE variable and
    Added DATE variable, this allows timestamp of "YYYY-MM-DD-HHSS".
    This means multiple backups per day can be stored.

--- scripts/docker_backup.sh
+++ scripts/docker_backup.sh
@@ -5,36 +5,38 @@ pushd ~/IOTstack
 [ -d ./backups ] || mkdir ./backups
 
 #create the list of files to backup
-echo "./docker-compose.yml" >list.txt
-echo "./services/" >>list.txt
-echo "./volumes/" >>list.txt
+echo "./docker-compose.yml" > list.txt
+echo "./services/" >> list.txt
+echo "./volumes/" >> list.txt
 
 #if influxdb folder exists then back it up
 if [ -d ./volumes/influxdb ]; then
-	./scripts/backup_influxdb.sh
-	echo "./backups/influxdb/" >>list.txt
+    ./scripts/backup_influxdb.sh
+    echo "./backups/influxdb/" >> list.txt
 fi
+
+DATE=$(date +"%Y-%m-%d_%H%M")
 
 echo "compressing stack folders"
 sudo tar -czf \
-	./backups/"backup-$(date +"%Y-%m-%d").tar.gz" \
-	--exclude=./volumes/influxdb/* \
-	-T list.txt
+./backups/"backup-$DATE.tar.gz" \
+--exclude=./volumes/influxdb/* \
+-T list.txt
 
 rm list.txt
 
-echo "backup saved to ./backups/"backup-$(date +"%Y-%m-%d").tar.gz""
+echo "backup saved to ./backups/backup-$DATE.tar.gz"
 
-du -h ./backups/"backup-$(date +"%Y-%m-%d").tar.gz"
+du -h ./backups/"backup-$DATE.tar.gz"
 
 if [ -f ./backups/dropbox ]; then
-	echo "uploading to dropbox"
-	~/Dropbox-Uploader/dropbox_uploader.sh upload ./backups/"backup-$(date +"%Y-%m-%d").tar.gz" /IOTstackBU/
+    echo "uploading to dropbox"
+    ~/Dropbox-Uploader/dropbox_uploader.sh upload ./backups/"backup-$DATE.tar.gz" /IOTstackBU/
 fi
 
 if [ -f ./backups/rclone ]; then
-	echo "uploading to Google Drive"
-	rclone -P copy ./backups/"backup-$(date +"%Y-%m-%d").tar.gz" gdrive:/IOTstackBU/
+    echo "uploading to Google Drive"
+    rclone -P copy ./backups/"backup-$DATE.tar.gz" gdrive:/IOTstackBU/
 fi
 
 ls -t1 ./backups/backup* | tail -n +6 | sudo xargs rm -f
