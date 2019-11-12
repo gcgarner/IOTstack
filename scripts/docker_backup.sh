9b18abc84aa93629b5ae7832f9dcf36f2ef3d9fc
877dev <46575282+877dev@users.noreply.github.com>
2019-11-12 20:11:37 +0000

    Update docker_backup.sh to fix permissions
    When log.txt is created it's got root permissions, so cannot be written to by the script.
    I added a line to change permission to user pi.
    
    Problem - the old backup files cannot be removed: permission denied.

--- scripts/docker_backup.sh
+++ scripts/docker_backup.sh
@@ -5,50 +5,54 @@ pushd ~/IOTstack
 [ -d ./backups ] || mkdir ./backups
 
 #create the list of files to backup
-echo "./docker-compose.yml" > list.txt
-echo "./services/" >> list.txt
-echo "./volumes/" >> list.txt
+echo "./docker-compose.yml" >list.txt
+echo "./services/" >>list.txt
+echo "./volumes/" >>list.txt
 
 #if influxdb folder exists then back it up
 if [ -d ./volumes/influxdb ]; then
-    ./scripts/backup_influxdb.sh
-    echo "./backups/influxdb/" >> list.txt
+	./scripts/backup_influxdb.sh
+	echo "./backups/influxdb/" >>list.txt
 fi
 
-#setup variables 
+#setup variables
 logfile=./backups/log.txt
 backupfile="backup-$(date +"%Y-%m-%d_%H%M").tar.gz"
 
 #compress the backups folders to archive
 echo "compressing stack folders"
 sudo tar -czf \
-./backups/$backupfile \
---exclude=./volumes/influxdb/* \
--T list.txt
+	./backups/$backupfile \
+	--exclude=./volumes/influxdb/* \
+	-T list.txt
 
 rm list.txt
 
+#setup permissions
+sudo chown pi:pi ./backups/backup*
+sudo chown pi:pi ./backups/log.txt
+
 #create log file and add the backup file to it
 echo "backup saved to ./backups/$backupfile"
-sudo touch $logfile
-echo $backupfile | sudo tee $logfile -a
+touch $logfile
+echo $backupfile >>$logfile
 
 #show size of archive file
 du -h ./backups/$backupfile
 
 #upload to cloud
 if [ -f ./backups/dropbox ]; then
-    echo "uploading to dropbox"
-    ~/Dropbox-Uploader/dropbox_uploader.sh upload ./backups/$backupfile /IOTstackBU/
+	echo "uploading to dropbox"
+	~/Dropbox-Uploader/dropbox_uploader.sh upload ./backups/$backupfile /IOTstackBU/
 fi
 
 if [ -f ./backups/rclone ]; then
-    echo "uploading to Google Drive"
-    rclone -P copy ./backups/$backupfile gdrive:/IOTstackBU/
+	echo "uploading to Google Drive"
+	rclone -P copy ./backups/$backupfile gdrive:/IOTstackBU/
 fi
 
 #remove older backup files
-ls -t1 ./backups/backup* | tail -n +6 | sudo xargs rm -f
+ls -t1 ./backups/backup* | tail -n +6 | xargs rm -f
 echo "last five backup files are saved in ~/IOTstack/backups"
 
 popd
