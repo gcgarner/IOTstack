gcgarner/IOTstack

commit 53d9cde49f6ab5504134720ac571f290e37b9ce5

PR:877dev. Added timestamp to backups, keep last 5, minor tweaks to 877devs code

scripts/docker_backup.sh
@@ -5,50 +5,52 @@ pushd ~/IOTstack
 [ -d ./backups ] || mkdir ./backups
 
 #create the list of files to backup
-echo "./docker-compose.yml" > list.txt
-echo "./services/" >> list.txt
-echo "./volumes/" >> list.txt
+echo "./docker-compose.yml" >list.txt
+echo "./services/" >>list.txt
+echo "./volumes/" >>list.txt
 
 #if influxdb folder exists then back it up
 if [ -d ./volumes/influxdb ]; then
-    ./scripts/backup_influxdb.sh
-    echo "./backups/influxdb/" >> list.txt
+	./scripts/backup_influxdb.sh
+	echo "./backups/influxdb/" >>list.txt
 fi
 
-#setup variables 
+#setup variables
 logfile=./backups/log.txt
 backupfile="backup-$(date +"%Y-%m-%d_%H%M").tar.gz"
 
 #compress the backups folders to archive
 echo "compressing stack folders"
 sudo tar -czf \
-./backups/$backupfile \
---exclude=./volumes/influxdb/* \
--T list.txt
+	./backups/$backupfile \
+	--exclude=./volumes/influxdb/* \
+	-T list.txt
 
 rm list.txt
 
+sudo chown pi:pi ./backups/backup*
+
 #create log file and add the backup file to it
 echo "backup saved to ./backups/$backupfile"
-sudo touch $logfile
-echo $backupfile | sudo tee $logfile -a
+touch $logfile
+echo $backupfile >>$logfile
 
 #show size of archive file
 du -h ./backups/$backupfile
 
 #upload to cloud
 if [ -f ./backups/dropbox ]; then
-    echo "uploading to dropbox"
-    ~/Dropbox-Uploader/dropbox_uploader.sh upload ./backups/$backupfile /IOTstackBU/
+	echo "uploading to dropbox"
+	~/Dropbox-Uploader/dropbox_uploader.sh upload ./backups/$backupfile /IOTstackBU/
 fi
 
 if [ -f ./backups/rclone ]; then
-    echo "uploading to Google Drive"
-    rclone -P copy ./backups/$backupfile gdrive:/IOTstackBU/
+	echo "uploading to Google Drive"
+	rclone -P copy ./backups/$backupfile gdrive:/IOTstackBU/
 fi
 
 #remove older backup files
-ls -t1 ./backups/backup* | tail -n +6 | sudo xargs rm -f
+ls -t1 ./backups/backup* | tail -n +6 | xargs rm -f
 echo "last five backup files are saved in ~/IOTstack/backups"
 
 popd

commit 74824d7d142017d28594bf61e2238fa5d20ea455

Corrects backup script permissions problem introduced by commit 53d9cde
docker_backup.sh contains embedded "sudo" commands so the intention
seems to be that the script should not be run as root. The unprivileged
"touch" command to create the log file in the backups directory fails
because the directory has root ownership. The backup files (.tar.gz) are
given user ownership so the log file should probably have matching user
ownership. This PR reinstates "sudo touch $logfile" but then changes
$logfile ownership to pi:pi before the backup file name is echoed to
the log file.

scripts/docker_backup.sh
@@ -32,7 +32,8 @@ sudo chown pi:pi ./backups/backup*
 
 #create log file and add the backup file to it
 echo "backup saved to ./backups/$backupfile"
-touch $logfile
+sudo touch $logfile
+sudo chown pi:pi $logfile
 echo $backupfile >>$logfile
 
 #show size of archive file