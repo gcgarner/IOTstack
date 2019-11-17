567a476d61f632927f148f2a6b679c53e4b52feb
877dev <46575282+877dev@users.noreply.github.com>
2019-11-17 14:12:03 +0000

    Update docker_backup.sh
    Increased local backups from 5 to 7
    Fixed local files not being deleted : permission denied
    Improvements to Dropbox - now last 7 files are kept and the rest deleted

--- scripts/docker_backup.sh
+++ scripts/docker_backup.sh
@@ -16,7 +16,7 @@ if [ -d ./volumes/influxdb ]; then
 fi
 
 #setup variables
-logfile=./backups/log.txt
+logfile=./backups/log_local.txt
 backupfile="backup-$(date +"%Y-%m-%d_%H%M").tar.gz"
 
 #compress the backups folders to archive
@@ -28,9 +28,10 @@ sudo tar -czf \
 
 rm list.txt
 
+#set permission for backup files
 sudo chown pi:pi ./backups/backup*
 
-#create log file and add the backup file to it
+#create local logfile and append the latest backup file to it
 echo "backup saved to ./backups/$backupfile"
 sudo touch $logfile
 sudo chown pi:pi $logfile
@@ -39,19 +40,53 @@ echo $backupfile >>$logfile
 #show size of archive file
 du -h ./backups/$backupfile
 
-#upload to cloud
+#remove older local backup files
+ls -t1 ./backups/backup* | tail -n +8 | sudo xargs rm -f
+echo "last seven local backup files are saved in ~/IOTstack/backups"
+
+
+
+#cloud related - dropbox
 if [ -f ./backups/dropbox ]; then
+
+	#setup variables
+	dropboxfolder=/IOTstackBU
+	dropboxuploader=~/Dropbox-Uploader/dropbox_uploader.sh
+	dropboxlog=./backups/log_dropbox.txt
+
+	#upload new backup to dropbox
 	echo "uploading to dropbox"
-	~/Dropbox-Uploader/dropbox_uploader.sh upload ./backups/$backupfile /IOTstackBU/
+	$dropboxuploader upload ./backups/$backupfile $dropboxfolder
+
+	#list older files to be deleted from cloud (exludes last 7)
+	echo "getting older filenames to be deleted from cloud"
+	files=$($dropboxuploader list $dropboxfolder | awk {' print $3 '} | tail -n +2 | head -n -7)
+
+	#write files to be deleted to logfile
+	sudo touch $dropboxlog
+	sudo chown pi:pi $dropboxlog
+	echo $files | tr " " "\n" >$dropboxlog
+
+	#delete files from cloud as per logfile
+	echo "deleting files from cloud - last 7 files are kept"
+	echo "if less than 7 files are in cloud you wil see FAILED message below"
+	input=$dropboxlog
+	while IFS= read -r file
+	do
+	    $dropboxuploader delete $dropboxfolder/$file
+	done < "$input"
+
+	echo "deleted from cloud" >>$dropboxlog
+
 fi
 
+
+#cloud related - google drive
 if [ -f ./backups/rclone ]; then
 	echo "uploading to Google Drive"
 	rclone -P copy ./backups/$backupfile gdrive:/IOTstackBU/
 fi
 
-#remove older backup files
-ls -t1 ./backups/backup* | tail -n +6 | sudo xargs rm -f
-echo "last five backup files are saved in ~/IOTstack/backups"
+
 
 popd
