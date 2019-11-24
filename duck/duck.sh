59ad87db56af1f3c20eb7c8114ee12bc5381d7f7
hepelayo <hepelayo@gmail.com>
2019-11-24 09:39:56 +0100

    duck: added variables for easier config

--- duck/duck.sh
+++ duck/duck.sh
@@ -1,2 +1,6 @@
 #!/bin/bash
-curl -k -o /var/log/duck.log "https://www.duckdns.org/update?domains=exampledomain&token=a7c4d0ad-114e-40ef-ba1d-d217904a50f2&ip="
+# Your comma-separated domains list
+DOMAINS="YOUR_DOMAINS"
+# Your DuckDNS Token
+DUCKDNS_TOKEN="YOUR_DUCKDNS_TOKEN"
+curl -k -o /var/log/duck.log "https://www.duckdns.org/update?domains=${DOMAINS}&token=${DUCKDNS_TOKEN}&ip="
