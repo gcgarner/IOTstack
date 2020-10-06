a3e3c24be0b5bb2dbcf70c9ec4f413c04f4a905d
Slyke <steven.lawler777@gmail.com>
2020-10-05 22:38:29 -0700

    Added in link to mosquitto's security documentation

--- .templates/mosquitto/terminal.sh
+++ .templates/mosquitto/terminal.sh
@@ -2,5 +2,6 @@
 
 echo "you are about to enter the shell for mosquitto"
 echo "the documentation explains how to secure mosquitto with a username and password."
+echo "Security Documentation: https://github.com/SensorsIot/IOTstack/blob/master/docs/Containers/Mosquitto.md#security"
 
 docker exec -it mosquitto sh
