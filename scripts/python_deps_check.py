a9c40ff155c68e0674a5ecc702a791992e64691a
James Myatt <james@jamesmyatt.co.uk>
2020-12-31 15:40:29 +0000

    Fix to run without PyYAML

--- scripts/python_deps_check.py
+++ scripts/python_deps_check.py
@@ -1,34 +1,35 @@
+#!/usr/bin/python3
 import sys
-import yaml
-import blessed
-import ruamel.yaml
 
 if sys.argv[1] == "--pyyaml-version":
   try:
+    import yaml
     print("pyyaml", yaml.__version__)
     sys.exit(0)
   except SystemExit:
     sys.exit(0)
-  except:
+  except Exception:
     print("could not get pyyaml version")
     sys.exit(3)
 
 if sys.argv[1] == "--pyaml-version":
   try:
+    import ruamel.yaml
     print("ruamel.yaml", ruamel.yaml.__version__)
     sys.exit(0)
   except SystemExit:
     sys.exit(0)
-  except:
+  except Exception:
     print("could not get ruamel.yaml version")
     sys.exit(3)
 
 if sys.argv[1] == "--blessed-version":
   try:
+    import blessed
     print("blessed", blessed.__version__)
     sys.exit(0)
   except SystemExit:
     sys.exit(0)
-  except:
+  except Exception:
     print("could not get blessed version")
     sys.exit(3)
