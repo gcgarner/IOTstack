aedde3cf228cc5e07c9ba31bd9ac232f5aae6ee7
ukkopahis <95980324+ukkopahis@users.noreply.github.com>
2022-06-18 22:14:55 +0300

    buildstack_menu: fix error on empty compose-override.yml
    Fixes #433
    
    Also improves on exception reporting to include full stacktrace

--- scripts/buildstack_menu.py
+++ scripts/buildstack_menu.py
@@ -11,6 +11,7 @@ def main():
   import math
   import sys
   import subprocess
+  import traceback
   from deps.chars import specialChars, commonTopBorder, commonBottomBorder, commonEmptyLine, padText
   from deps.consts import servicesDirectory, templatesDirectory, volumesDirectory, buildCache, envFile, dockerPathOutput, servicesFileName, composeOverrideFile
   from deps.yaml_merge import mergeYaml
@@ -92,7 +93,7 @@ def main():
       return True
     except Exception as err: 
       print("Issue running build:")
-      print(err)
+      traceback.print_exc()
       input("Press Enter to continue...")
       return False
 
@@ -318,7 +319,7 @@ def main():
 
     except Exception as err: 
       print("There was an error rendering the menu:")
-      print(err)
+      traceback.print_exc()
       print("Press [Esc] to go back")
       return
 
@@ -405,13 +406,13 @@ def main():
                 menu[getMenuItemIndexByService(checkedMenuItem)][1]["issues"] = []
             except Exception as err:
               print("Error running checkForIssues on '%s'" % checkedMenuItem)
-              print(err)
+              traceback.print_exc()
               input("Press Enter to continue...")
           else:
             menu[getMenuItemIndexByService(checkedMenuItem)][1]["issues"] = []
         except Exception as err:
           print("Error running checkForIssues on '%s'" % checkedMenuItem)
-          print(err)
+          traceback.print_exc()
           input("Press any key to exit...")
           sys.exit(1)
 
@@ -437,7 +438,7 @@ def main():
             menu[getMenuItemIndexByService(menuItem[0])][1]["buildHooks"]["options"] = True
         except Exception as err:
           print("Error running checkForOptions on '%s'" % menuItem[0])
-          print(err)
+          traceback.print_exc()
           input("Press any key to exit...")
           sys.exit(1)
 
@@ -466,7 +467,7 @@ def main():
               exec(code, execGlobals, execLocals)
           except Exception as err:
             print("Error running PreBuildHook on '%s'" % checkedMenuItem)
-            print(err)
+            traceback.print_exc()
             input("Press Enter to continue...")
             try: # If the prebuild hook modified the docker-compose object, pull it from the script back to here.
               dockerComposeServicesYaml = execGlobals["dockerComposeServicesYaml"]
@@ -497,7 +498,7 @@ def main():
               exec(code, execGlobals, execLocals)
           except Exception as err:
             print("Error running PostBuildHook on '%s'" % checkedMenuItem)
-            print(err)
+            traceback.print_exc()
             input("Press Enter to continue...")
 
   def executeServiceOptions():
