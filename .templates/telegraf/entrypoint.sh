bbc510eeecd76ea58b033d2042edf98b7d68e8af
Phill Kelley <34226495+Paraphraser@users.noreply.github.com>
2023-06-24 15:02:01 +1000

    2023-06-24 Telegraf - master branch - PR 1 of 2
    Issue reported on
    [Discord](https://discord.com/channels/638610460567928832/638610461109256194/1121820918210121838)
    led to the discovery of several problems with Telegraf:
    
    1. Default configuration had been amended to:
    
    	- Add support for both InfluxDB 1.8 and InfluxDB 2; and
    	- Comment-out both InfluxDB outputs.
    
    	The practical consequences were:
    
    	- The Dockerfile `sed` command could not find `[outputs.influxdb]`,
    	so the `urls = ["http://influxdb:8086"]` could not be inserted, so
    	telegraf could not write to any database; and
    	- The container went into a restart loop.
    
    2. The default `entrypoint.sh` script had been amended to invoke
    telegraf via:
    
    	```
    	exec setpriv --reuid telegraf --init-groups "$@"
    	```
    
    	Inside the container, user "telegraf" is userID 999. Outside
    	container space, user 999 is not a member of the `docker` group so
    	it doesn't have access to `/var/run/docker.sock`.
    
    	The practical consequence was the telegraf process inside the
    	container endlessly complaining:
    
    	```
    	E! [inputs.docker] Error in plugin: permission denied while trying to connect to the Docker daemon socket at unix:///var/run/docker.sock
    	```
    
    Problem 1 has been addressed by removing the `sed` logic and adding
    `outputs.influxdb.conf` to the `auto_include` folder:
    
    ```
    [[outputs.influxdb]]
    urls = ["http://influxdb:8086"]
    ```
    
    This defaults to the `telegraf` database and is sufficient for telegraf
    to get going.
    
    Problem 2 has been addressed by not downgrading privileges. The
    alternative of changing the documentation to require the user to add
    userID 999 to the `docker` group is sub-optimal and not really in the
    spirit of IOTstack where, to the maximum extent possible, containers
    should "just work".
    
    Signed-off-by: Phill Kelley <34226495+Paraphraser@users.noreply.github.com>

--- .templates/telegraf/entrypoint.sh
+++ .templates/telegraf/entrypoint.sh
@@ -9,10 +9,28 @@ fi
 U="$(id -u)"
 T="/etc/telegraf"
 if [ "$U" = '0' -a -d "$T" ]; then
+   echo "Performing IOTstack self repair"
    rsync -arp --ignore-existing /${IOTSTACK_DEFAULTS_DIR}/ "$T"
    chown -R "$U:$U" "$T"
 fi
 
+if [ $EUID -eq 0 ]; then
+
+    # Allow telegraf to send ICMP packets and bind to privliged ports
+    setcap cap_net_raw,cap_net_bind_service+ep /usr/bin/telegraf || echo "Failed to set additional capabilities on /usr/bin/telegraf"
+
+    # note: at this point, the default version of this file runs:
+    #
+    #          exec setpriv --reuid telegraf --init-groups "$@"
+    #
+    #       Inside the container, user "telegraf" is userID 999, which
+    #       isn't a member of the "docker" group outside container-space
+    #       so the practical effect of downgrading privileges in this
+    #       way is to deny access to /var/run/docker.sock, and then you
+    #       get a mess. It's not clear whether the setcap is necessary
+    #       on a Raspberry Pi but it has been left in place in case it
+    #       turns out to be useful in other Docker environments.
+
+fi
+
 exec "$@"
-
-
